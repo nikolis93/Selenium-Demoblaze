a7e4a98aaa88f459d66de781eff5095014631754
nikolis93 <nick.mixalakis@gmail.com>
2023-09-03 13:29:00 +0300

    The main class of the project

---
+++ Demoblaze.java
@@ -0,0 +1,22 @@
+package nikos.selenium_testing;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ *
+ * @author nikolis93
+ */
+public class Demoblaze {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\30690\\Desktop\\selenium apps\\geckodriver-v0.33.0-win64\\geckodriver.exe");
+
+        WebDriver driver = new FirefoxDriver();
+
+        //Actions.SignUp(driver, "https://www.demoblaze.com/");
+        Actions.LoginIn(driver, "https://www.demoblaze.com/");
+       // Actions.LogOut(driver, "https://www.demoblaze.com/"); //not working need to check it again
+    }
+
+}
