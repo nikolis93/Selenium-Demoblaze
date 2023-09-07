5f3aa625ae992a64c034a2c68162cfab3345ab24
nikolis93 <nick.mixalakis@gmail.com>
2023-09-07 21:19:40 +0300

    Update Actions.java
    added the add to cart functionality

--- src/Actions.java
+++ src/Actions.java
@@ -1,6 +1,7 @@
 package nikos.selenium_testing;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.Alert;
@@ -18,7 +19,63 @@ import org.openqa.selenium.support.ui.Wait;
  */
 public class Actions {
 
-    public static void LogOut(WebDriver driver, String url) {        
+    private static void AddToCart(WebDriver driver, String name) {
+        System.out.println("ADD TO CART CALLED for: " + name);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        WebElement body = driver.findElement(By.id("tbodyid"));
+        WebElement addToCartButton = body.findElement(By.linkText("Add to cart"));
+        addToCartButton.click();
+        
+        Wait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(2))
+                .pollingEvery(Duration.ofMillis(300))
+                .ignoring(ElementNotInteractableException.class);
+        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+        String alertText = alert.getText();
+
+        if (alertText.equalsIgnoreCase("Product added.")) {
+            System.out.println("Product added to cart!");
+        }
+
+    }
+
+    public static void ProductsList(WebDriver driver, String url) {
+        ArrayList<WebElement> products = new ArrayList<>();
+
+        driver.getPageSource();
+        WebElement grid = driver.findElement(By.id("tbodyid"));
+        List<WebElement> productsGrid = grid.findElements(By.className("card-block"));
+
+        for (WebElement product : productsGrid) {
+            try {
+                System.out.println("product: " + product.getText());
+                WebElement el = product.findElement(By.tagName("a"));
+                products.add(el);
+                System.out.println("el: " + el.getText());
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                break;
+            }
+        }
+        String productName = "";
+        System.out.println("products size: " + products.size());
+        for (WebElement product : products) {
+            try {
+                productName = product.getText();
+
+                if (productName.equalsIgnoreCase("Nokia lumia 1520")) {
+                    product.click();
+                    AddToCart(driver, productName);
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+       
+    }
+
+    public static void LogOut(WebDriver driver, String url) {
+        //driver.get(url);
         String html = driver.getPageSource();
         try {
             WebElement logout = driver.findElement(By.id("logout2"));
@@ -34,7 +91,7 @@ public class Actions {
 
     }
 
-    public static void LoginIn(WebDriver driver, String url) {
+    public static WebDriver LoginIn(WebDriver driver, String url) {
         driver.get(url);
         WebElement login = driver.findElement(By.id("login2"));
         login.click();
@@ -68,9 +125,10 @@ public class Actions {
                     }
                 }
             } catch (Exception e) {
-
+                System.out.println(e.getMessage());
             }
         }
+        return driver;
     }
 
     public static void SignUp(WebDriver driver, String url) {//this function automates the sign up process
@@ -109,7 +167,7 @@ public class Actions {
                     }
                 }
             } catch (Exception e) {
-
+                System.out.println(e.getMessage());
             }
         }
     }
